fec0c7eee73ab9ff19302ea54bb3e46df61869bb
guptaaashu <guptaaashuid@gmail.com>
2019-12-11 16:05:33 +0530

    changes

--- hay/settings.py
+++ hay/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
 
