314043bfa59839e3fad0fcdb4a0666d86df04b0b
guptaaashu <guptaaashuid@gmail.com>
2019-12-11 18:39:33 +0530

    changes

--- hay/settings.py
+++ hay/settings.py
@@ -153,8 +153,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
 
